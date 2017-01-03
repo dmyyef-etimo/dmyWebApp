a6d16c4ab66356fb2271c2d2a43866be60184c3d
Xyi Tebe <xyi_tebe@hotmail.com>
2017-01-03 18:54:00 +0100

    Add project files.

---
+++ dmyWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dmyWebApp.MvcApplication" Language="C#" %>
---
+++ dmyWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dmyWebApp.Startup))]
+namespace dmyWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
