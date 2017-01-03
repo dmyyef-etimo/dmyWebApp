2f428d4044dccb6b427beac64e2e338927e7026e
Xyi Tebe <xyi_tebe@hotmail.com>
2017-01-03 23:14:30 +0100

    Customer

--- dmyWebApp/Controllers/MoviesController.cs
+++ dmyWebApp/Controllers/MoviesController.cs
@@ -9,12 +9,30 @@ namespace dmyWebApp.Controllers
         public ActionResult Random()
         {
             var movie = new Movie {Name = "Shrack!"};
-//            return View(movie);
-            //  return Content(content: "Hello World!");
-            // return HttpNotFound(statusDescription: "not found!");
-            // return new EmptyResult();
-            return RedirectToAction(actionName: "Index", controllerName: "Home",
-                routeValues: new {page = 1, sortBy = "page"});
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content($"id = {id}");
+        }
+
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (pageIndex.HasValue == false)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content($"page index = {pageIndex}, sort by = {sortBy}");
+        }
+
+        [Route(template: "movies/released/{year:regex(\\d{4}):range(1900,2050)}/{month:regex(\\d{1,2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content($"{year}/{month:00}");
         }
     }
 }
---
+++ dmyWebApp/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace dmyWebApp.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
